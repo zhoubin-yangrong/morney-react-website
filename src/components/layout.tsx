cc27d8f6fbd17a75c4ce7ff31587ceebef6b0bfa
白斌 <990169111@qq.com>
2021-10-31 20:40:20 +0800

    Money.tsx 之 整个-UI基本完成

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,12 +11,15 @@ const Main = styled.div`
   //border: 1px solid greenyellow;
   flex-grow: 1;
   overflow: auto;
+  display: flex;
+  flex-direction: column;
+  
 `
 
 const Layout = (props:any)=>{
     return(
         <Wrapper>
-            <Main>
+            <Main className={props.className}>
                 {props.children}
             </Main>
             <Nav/>
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -4,6 +4,11 @@ import styled from "styled-components";
 
 const TagsSection = styled.section`
   background: #FFFFFF;padding: 0 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items:flex-start;
   > ol{display: flex;margin: 0 -12px 17px;
     > li{
       padding: 4px 17px;margin: 0 12px;background: #D9D9D9;border-radius: 18px;font-size: 14px;
@@ -115,9 +120,12 @@ const NumberPadSection = styled.section`
     
   }
 `
+const MyLayout = styled(Layout)`
+  //border: 1px solid red;
+`
 function Money() {
     return(
-        <Layout>
+        <MyLayout className="xxx">
             <TagsSection>
                 <ol>
                     <li>衣</li>
@@ -161,7 +169,7 @@ function Money() {
                 </div>
 
             </NumberPadSection>
-        </Layout>
+        </MyLayout>
         // <Wrapper>
         //     <Main><h2>记账页</h2>;</Main>
         //     <Nav/>
