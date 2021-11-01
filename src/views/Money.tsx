8ba01444ab1f2b8ff584f4854bfc442420f92632
白斌 <990169111@qq.com>
2021-11-01 23:45:40 +0800

    重构表单数据收集

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -17,32 +17,33 @@ function Money() {
         category:"-" as (Category),
         amount:0
     })
+   /* 类型合并(如果全复制selected的类型 传参会有数量问题)
+    Partial 合并的意思
+    typeof selected 继承此类*/
+    const onChange = (obj:Partial<typeof selected>)=>{
+        setSelected({...selected,...obj})
+    }
     return(
         <MyLayout className="xxx">
             {selected.tags.join("")}
             <hr/>
             {selected.note}
+            <hr/>
+            {selected.category}
+            <hr/>
+            {selected.amount}
+            <hr/>
 
             <TagsSection value={selected.tags}
-                         onChange={(tags)=>{
-                setSelected({...selected,tags})}}
-            />
+                         onChange={(tags)=>{onChange({tags})}}/>
             <NoteSection value={selected.note}
-                         onChange={(note)=>{
-                             setSelected({...selected,note})}}/>
+                         onChange={(note)=>{onChange({note})}}/>
             <CategorySection  value={selected.category}
-                              onChange={(category)=>{
-                                  setSelected({...selected,category})}}/>
+                              onChange={(category)=>{onChange({category})}}/>
             <NumberPadSection value={selected.amount}
-                              onOk={()=>{
-                                  console.log("ok提交")}}
-                              onChange={(amount)=>{
-                                  setSelected({...selected,amount})}}/>
+                              onOk={()=>{console.log("ok提交")}}
+                              onChange={(amount)=>{onChange({amount})}}/>
         </MyLayout>
-        // <Wrapper>
-        //     <Main><h2>记账页</h2>;</Main>
-        //     <Nav/>
-        // </Wrapper>
     )
 }
 export default Money
