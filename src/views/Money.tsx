4abf9b344fca8049a50473cd8ab6deb914aa78aa
白斌 <990169111@qq.com>
2021-11-01 12:47:31 +0800

    完成标签的选择和新增

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -13,13 +13,6 @@ function Money() {
     return(
         <MyLayout className="xxx">
             <TagsSection>
-                <ol>
-                    <li>衣</li>
-                    <li>食</li>
-                    <li>住</li>
-                    <li>行</li>
-                </ol>
-                <button>新增标签</button>
             </TagsSection>
             <NoteSection>
                 <label>备注</label>
--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,15 +1,18 @@
 import styled from "styled-components";
-
-const TagsSection = styled.section`
+import React, {useState} from "react";
+const Wrapper = styled.section`
   background: #FFFFFF;padding: 0 16px;
   flex-grow: 1;
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
   align-items:flex-start;
-  > ol{display: flex;margin: 0 -12px 17px;
+  > ol{display: flex;margin: 0 -12px 17px;flex-wrap: wrap;
     > li{
-      padding: 4px 17px;margin: 0 12px;background: #D9D9D9;border-radius: 18px;font-size: 14px;
+      &.selected{
+        background-color: skyblue;
+      }
+      padding: 4px 17px;margin: 5px 12px;background: #D9D9D9;border-radius: 18px;font-size: 14px;
     }
   }
   > button{
@@ -17,4 +20,35 @@ const TagsSection = styled.section`
     border-bottom: 1px solid #333;padding: 0 2px;margin-bottom: 12px;
   }
 `
+const TagsSection: React.FC = ()=>{
+    const [tag,setTag] = useState<string[]>(["衣","食","住","行","花","羊绒吃屎"])
+    const onAddTag =()=>{
+        const tagName =window.prompt("新增标签")
+        if (tagName!==null){
+            setTag([...tag,tagName])
+        }else {
+            alert("玩呢?")
+        }
+    }
+    const onToggleTags=(tag:string)=>{
+        const index:number = selected.indexOf(tag)
+        if (index>=0){
+            setSelected(selected.filter(t=>t!==tag))
+        }else {
+            setSelected([...selected,tag])
+        }
+    }
+    const [selected,setSelected] = useState<string[]>([])
+    return(
+        <Wrapper>
+            <ol>
+                {tag.map(tag=>{
+                    return <li key={tag} onClick={()=>{onToggleTags(tag)}} className={selected.indexOf(tag)>=0?"selected":""} >{tag}</li>
+                })}
+            </ol>
+            <button onClick={()=>{onAddTag()}}>新增标签</button>
+        </Wrapper>
+    )
+}
+
 export {TagsSection}
