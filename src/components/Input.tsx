3c5bca12fdaafd354c2d5381f156755f1f984e8e
白斌 <990169111@qq.com>
2021-11-03 15:05:06 +0800

    创建单独的 Input 组件

---
+++ src/components/Input.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+const Label = styled.label`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    >span{
+      white-space: nowrap;
+    }
+    > input{
+      margin-left:16px ;
+      display: block;
+      height: 72px;
+      width: 100%;
+      border: none;
+      background: none;
+  }
+`
+type Props = {
+    label:string
+} & React.InputHTMLAttributes<HTMLInputElement>
+const Input:React.FC<Props> = (props)=>{
+    const {label,children,...reset}=props //抽离部分参数其余的自动拷属性参数到...reset中
+    return(
+        <Label>
+            <span>{label}</span>
+            <input {...reset}/>
+        </Label>
+    )
+}
+export {Input}
+
